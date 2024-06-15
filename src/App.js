dcaca7d19dd2cfcf8e6c47b081243a18ed60af93
Yaman Aswal <yaman@appsquad.com>
2024-06-15 13:55:12 +0530

    INIT COMMIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 300;
+  background-color: blue;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`
+
+const Input = styled.input`
+  font-size: 1.4rem;
+  padding: 0.8rem 1.2rem;
+  font-weight: 500;
+  border: 1px #ddd;
+  border-radius: 5px;
+`
+
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 20px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <H1> Wild Osasis</H1>
+      <Button>CLick Here</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
   );
 }
 
