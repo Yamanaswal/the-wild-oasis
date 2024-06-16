ee7da7c631f03c8043029204039f534b7ce03b4b
Yaman Aswal <yaman@appsquad.com>
2024-06-16 14:58:50 +0530

    Style component completed

---
+++ src/ui/Input.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  font-size: 1.4rem;
+  border: 1px var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+`
+
+export default Input;
