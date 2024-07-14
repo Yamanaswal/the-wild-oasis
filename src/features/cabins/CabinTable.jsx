4aa8059cbd1c3228800830b2aa0d3a0966b70ca4
Yaman Aswal <yaman@appsquad.com>
2024-07-14 11:08:55 +0530

    code updated - 07-14-2024

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,28 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 
 function CabinTable() {
+
+  const [searchParams] = useSearchParams();
+  const discount = searchParams.get("discount") || "all";
+  console.log("DISCOUNT: ", discount);
 
   const {
     isLoading,
@@ -32,6 +21,38 @@ function CabinTable() {
     queryKey: ["cabins"],
     queryFn: getCabins
   });
+
+  if (cabins) {
+
+    // 1) Filter
+    let filterCabins;
+    var sortedCabins;
+
+    if (discount === "all") {
+      filterCabins = cabins;
+    }
+    if (discount === "no-discount") {
+      filterCabins = cabins.filter(cabinItem => {
+        return cabinItem.discount === 0;
+      });
+    }
+    if (discount === "with-discount") {
+      filterCabins = cabins.filter(cabinItem => {
+        return cabinItem.discount > 0;
+      });
+    }
+
+    // 2) SORT
+    const sortBy = searchParams.get("sortBy") || "startDate-asc";
+    const [field, direction] = sortBy.split("-");
+    const modifier = direction === "asc" ? 1 : -1;
+    console.log("filterCabins 4", filterCabins);
+    sortedCabins = filterCabins.sort(
+      function (a, b) {
+        return (a[field] - b[field]) * modifier;
+      }
+    );
+  }
 
   if (isLoading) {
     return (<Spinner />)
@@ -50,7 +71,9 @@ function CabinTable() {
         </Table.Header>
 
         {/* Cabin Listing Here. - Render Prop Pattern. */}
-        <Table.Body data={cabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
+        <Table.Body
+          data={sortedCabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
 
       </Table>
     </Menus>
