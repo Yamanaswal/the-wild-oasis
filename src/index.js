f4e73b202078074bffd4585c759ce940e69a450e
Yaman Aswal <yaman@appsquad.com>
2024-07-14 13:01:44 +0530

    Code Updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
